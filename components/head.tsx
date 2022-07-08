f35e2fc38782df26fbacee9f5aba8d16f93cd8c0
amalasyrafs <57763629+amalasyrafs@users.noreply.github.com>
2022-07-08 13:08:45 +0800

    initial commit

---
+++ components/head.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+const HTMLHead = () => {
+  return (
+    <Head>
+      <title>Portfolio | amalasyrafs</title>
+      <meta name="description" content="Personal website of Amaluddin Asyraf" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
+};
+
+export default HTMLHead;
