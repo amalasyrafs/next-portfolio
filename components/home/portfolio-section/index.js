d76fbe293549e9cdf19dc88d903ac25ebc2d2f3f
amalasyrafs <amaluddinasyraf@gmail.com>
2023-07-02 13:57:23 +0800

    working portfolio animation section

--- components/home/portfolio-section/index.js
+++ components/home/portfolio-section/index.js
@@ -1,9 +1,13 @@
 import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import { useIsomorphicLayoutEffect } from "@/hooks/useIsomorphicLayoutEffect";
 import useOnScreen from "@/hooks/useOnScreen";
 import Portfolio from "./portfolio";
 
 export default function PortfolioSection({ setSection }) {
   const ref = useRef(null);
+  const awardCardsRef = useRef([]);
   const isVisible = useOnScreen(ref);
 
   useEffect(() => {
@@ -11,6 +15,69 @@ export default function PortfolioSection({ setSection }) {
       setSection(1);
     }
   }, [isVisible]);
+
+  // useIsomorphicLayoutEffect(() => {
+  //   const awards = ref.current;
+  //   const awardCards = awardCardsRef.current;
+
+  //   // Set up initial positions of the cards
+  //   gsap.set(awardCards, { y: 100, opacity: 0 });
+
+  //   // Animate each card when it enters the viewport
+  //   awardCards.forEach((card, i) => {
+  //     ScrollTrigger.create({
+  //       trigger: card,
+  //       onEnter: () => {
+  //         gsap.to(card, {
+  //           duration: 0.8,
+  //           y: 0,
+  //           opacity: 1,
+  //           ease: "power3.out",
+  //           delay: i * 0.1,
+  //         });
+  //       },
+  //     });
+  //   });
+
+  //   // Animate the awards section heading
+  //   ScrollTrigger.create({
+  //     trigger: awards,
+  //     start: "top 80%",
+  //     onEnter: () => {
+  //       gsap.to(awards.querySelector("h2"), {
+  //         duration: 0.5,
+  //         opacity: 1,
+  //         y: 0,
+  //         ease: "power2.out",
+  //       });
+  //     },
+  //   });
+
+  //   // Animate the card container when it enters the viewport
+  //   ScrollTrigger.create({
+  //     trigger: awards,
+  //     start: "top 70%",
+  //     end: "bottom 30%",
+  //     onEnter: () => {
+  //       gsap.to(awardCards, {
+  //         duration: 0.8,
+  //         y: -100,
+  //         opacity: 0,
+  //         stagger: 0.1,
+  //         ease: "power3.out",
+  //       });
+  //     },
+  //     onLeaveBack: () => {
+  //       gsap.to(awardCards, {
+  //         duration: 0.8,
+  //         y: 0,
+  //         opacity: 1,
+  //         stagger: 0.1,
+  //         ease: "power3.out",
+  //       });
+  //     },
+  //   });
+  // }, []);
 
   return (
     <section
@@ -36,6 +103,41 @@ export default function PortfolioSection({ setSection }) {
         <p className={"py-2 font-sans text-sm"}>
           Selected works that I have worked on:
         </p>
+
+        {/* <Portfolio /> */}
+        {/* award-cards */}
+        {/* <div className="py-4">
+          <div
+            ref={(el) => (awardCardsRef.current[0] = el)}
+            className="box sticky top-4 h-[80vh] w-full -translate-y-[20%] scale-[60%] rounded-xl bg-invoke-blue hover:shadow-lg"
+          >
+            <h1>helloworld</h1>
+          </div>
+          <div
+            ref={(el) => (awardCardsRef.current[1] = el)}
+            className="box sticky top-4 h-[80vh] w-full -translate-y-[110%] scale-[65%] rounded-xl bg-invoke-brown hover:shadow-lg"
+          >
+            <h1>helloworld</h1>
+          </div>
+          <div
+            ref={(el) => (awardCardsRef.current[2] = el)}
+            className="box sticky top-4 h-[80vh] w-full -translate-y-[200%] scale-[70%] rounded-xl bg-invoke-maroon hover:shadow-lg"
+          >
+            <h1>helloworld</h1>
+          </div>
+          <div
+            ref={(el) => (awardCardsRef.current[3] = el)}
+            className="box sticky top-4 h-[80vh] w-full -translate-y-[290%] scale-[75%] rounded-xl bg-invoke-decoris hover:shadow-lg"
+          >
+            <h1>helloworld</h1>
+          </div>
+          <div
+            ref={(el) => (awardCardsRef.current[4] = el)}
+            className="box sticky top-4 h-[80vh] w-full -translate-y-[380%] scale-[80%] rounded-xl bg-invoke-meniaga hover:shadow-lg"
+          >
+            <h1>helloworld</h1>
+          </div>
+        </div> */}
 
         <Portfolio />
       </div>
--- components/home/portfolio-section/portfolio.js
+++ components/home/portfolio-section/portfolio.js
@@ -6,51 +6,120 @@ import { useIsomorphicLayoutEffect } from "@/hooks/useIsomorphicLayoutEffect";
 gsap.registerPlugin(ScrollTrigger);
 
 export default function Portfolio() {
-  const main = useRef();
+  const awardsRef = useRef();
+  const containerRef = useRef();
+  const childRefs = useRef([]);
 
   useIsomorphicLayoutEffect(() => {
-    const ctx = gsap.context((self) => {
-      const boxes = self.selector(".box");
-      // console.log(boxes);
+    // const ctx = gsap.context((self) => {
+    //   const boxes = self.selector(".box");
+    //   // console.log(boxes);
 
-      const container = self.selector(".box-container")[0];
-      console.log(container);
-      gsap.to(container, {
-        // backgroundColor: "red",
-        scrollTrigger: {
-          trigger: container,
-          start: "top center",
-          end: "+=250",
-          scrub: 2,
-        },
-      });
+    //   const container = self.selector(".box-container")[0];
+    //   console.log(container);
+    //   // gsap.to(container, {
+    //   //   backgroundColor: "red",
 
-      boxes.forEach((box, index) => {
-        gsap.to(box, {
-          x: index % 2 === 0 ? 250 : -250,
-          // opacity: index === 4 && 0,
-          // scale: index === 4 && "100%",
-          // translateY: index === 4 ? "350%" : "290%",
-          scrollTrigger: {
-            trigger: container,
-            start: `top ${index * 10 + 600}px`,
-            end: "+=600",
-            scrub: true,
-          },
-        });
+    //   //   scrollTrigger: {
+    //   //     trigger: container,
+    //   //     start: "top center",
+    //   //     end: "+=250",
+    //   //     scrub: true,
+    //   //   },
+    //   // });
+
+    //   boxes.forEach((box, index) => {
+    //     gsap.to(box, {
+    //       x: index % 2 === 0 ? 250 : -250,
+    //       // opacity: index === 4 && 0,
+    //       // scale: index === 4 && "100%",
+    //       // translateY: index === 4 ? "350%" : "290%",
+    //       scrollTrigger: {
+    //         trigger: container,
+    //         start: `top ${index * 10 + 600}px`,
+    //         end: "+=600",
+    //         scrub: true,
+    //       },
+    //     });
+    //   });
+    // }, main);
+    // return () => ctx.revert();
+
+    const containerTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: containerRef.current,
+        start: "top center",
+        end: "center center",
+        scrub: true,
+        markers: true, // Remove this in production
+      },
+    });
+
+    // Create timeline for children animation
+    const childrenTimeline = gsap.timeline({
+      paused: true,
+    });
+
+    // Add animations to container timeline
+    containerTimeline.to(containerRef.current, {
+      // opacity: 0,
+      // xPercent: 10,
+    });
+    // Play children timeline when container timeline is complete
+
+    // Add animations to children timeline
+    childRefs.current.forEach((ref) => {
+      childrenTimeline.to(ref.current, {
+        // Add your animation for the children elements
+        opacity: 0,
       });
-    }, main);
-    return () => ctx.revert();
+    });
+
+    // Set up ScrollTrigger for children timeline
+    ScrollTrigger.create({
+      trigger: containerRef.current,
+      start: "top top",
+      end: "top top",
+      onEnter: () => {
+        containerTimeline.play(); // Play container timeline when user scrolls to center of container
+      },
+      onLeaveBack: () => {
+        containerTimeline.reverse(); // Reverse container timeline when user scrolls back
+      },
+    });
   }, []);
 
   return (
-    <div className="max-h-screen py-4" ref={main}>
-      <div className="box-container">
-        <div className="box sticky top-4 h-[80vh] w-full -translate-y-[20%] scale-[60%] rounded-xl bg-invoke-blue"></div>
-        <div className="box sticky top-4 h-[80vh] w-full -translate-y-[110%] scale-[65%] rounded-xl bg-invoke-brown"></div>
-        <div className="box sticky top-4 h-[80vh] w-full -translate-y-[200%] scale-[70%] rounded-xl bg-invoke-maroon"></div>
-        <div className="box sticky top-4 h-[80vh] w-full -translate-y-[290%] scale-[75%] rounded-xl bg-invoke-decoris"></div>
-        <div className="box sticky top-4 h-[80vh] w-full -translate-y-[380%] scale-[80%] rounded-xl bg-invoke-meniaga"></div>
+    <div className="py-4" ref={awardsRef}>
+      <div
+        ref={(el) => (childRefs.current[0] = el)}
+        className="box sticky top-4 h-[80vh] w-full -translate-y-[20%] scale-[60%] rounded-xl bg-invoke-blue hover:shadow-lg"
+      >
+        <h1>helloworld</h1>
+      </div>
+      <div
+        ref={(el) => (childRefs.current[1] = el)}
+        className="box sticky top-4 h-[80vh] w-full -translate-y-[110%] scale-[65%] rounded-xl bg-invoke-brown hover:shadow-lg"
+      >
+        <h1>helloworld</h1>
+      </div>
+      <div
+        ref={(el) => (childRefs.current[2] = el)}
+        className="box sticky top-4 h-[80vh] w-full -translate-y-[200%] scale-[70%] rounded-xl bg-invoke-maroon hover:shadow-lg"
+      >
+        <h1>helloworld</h1>
+      </div>
+      <div
+        ref={(el) => (childRefs.current[3] = el)}
+        className="box sticky top-4 h-[80vh] w-full -translate-y-[290%] scale-[75%] rounded-xl bg-invoke-decoris hover:shadow-lg"
+      >
+        <h1>helloworld</h1>
+      </div>
+      <div
+        ref={(el) => (childRefs.current[4] = el)}
+        className="box sticky top-4 h-[80vh] w-full -translate-y-[380%] scale-[80%] rounded-xl bg-invoke-meniaga hover:shadow-lg"
+      >
+        <h1>helloworld</h1>
       </div>
     </div>
   );
